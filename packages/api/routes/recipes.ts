120a4b6b2a2c174912e53b52d02bd9c80a14d027
Kyle Williams <kylemw1133@gmail.com>
2022-09-07 13:00:26 -0400

    simple day meal plan returned without formatting

--- packages/api/routes/recipes.ts
+++ packages/api/routes/recipes.ts
@@ -4,7 +4,34 @@ const router = express.Router();
 
 //GET /test
 router.get("/", (req, res, next) => {
-  res.send({string:"recipe"});
+    res.send({
+        "request":"recipes for the day",
+        "breakfast": 
+        {
+            "name":"Egg and Cheese",
+            "ingredients": [
+              "2 eggs",
+              "shredded cheese"
+            ]
+        },
+       "lunch":
+        {
+            "name":"Chicken Sandwich",
+            "ingredients": [
+              "one chicken breast",
+              "Burger Bun"
+            ]
+        },
+       "dinner":
+        {
+            "name":"Pasta Salad",
+            "ingredients": [
+              "Orzo Pasta",
+              "Assortment of veggies",
+              "shredded parmesan"
+            ]
+        },
+    });
 })
 
 export default router;
--- packages/web/src/routes/nutrition.tsx
+++ packages/web/src/routes/nutrition.tsx
@@ -1,11 +1,41 @@
 import React from "react";
+import { useState } from "react";
 import NavBar from "../components/navbar";
+import { getRecipe } from "../requests/recipe";
+import "../App.css";
+import "./nutrition.css";
 
 export default function Nutrition() {
+  const [message, setMessage] = useState("")
+  const recipeRequest = async () => {
+     const res = await getRecipe();
+     setMessage(JSON.stringify(res.data) ?? "bad request")
+   };
+
   return (
-    <main>
-      <h2>Nutrition</h2>
-      <NavBar />
-    </main>
+        <div className="App">
+            <NavBar />
+
+            <div className="recipe-container">
+                <div className="recipe-item">
+                    <h2>What are you feeling for...</h2>
+
+                    <label htmlFor="recipe-breakfast">Breakfast: </label>
+                    <input type="text" name="recipe-breakfast" />
+                    <br />
+                    <label htmlFor="recipe-lunch">Lunch: </label>
+                    <input type="text" name="recipe-lunch" />
+                    <br />
+                    <label htmlFor="recipe-dinner">Dinner: </label>
+                    <input type="text" name="recipe-dinner" />
+                    <br /><br />
+                    <button onClick={recipeRequest}>Find me recipes!</button>
+                </div>
+                <div className="recipe-item">
+                    <h2>Meal Plan for the day</h2>
+                    <div className="recipe-content" dangerouslySetInnerHTML={{__html: message}}></div>
+                </div>
+            </div>
+        </div>
   );
 }
