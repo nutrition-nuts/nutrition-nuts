593304cf121c45b50bcda830c41e8b6e56c4c209
JuliaPatzak <patzak.2@osu.edu>
2022-09-19 09:45:09 -0400

    local storage and buttons

--- packages/web/src/routes/profile.tsx
+++ packages/web/src/routes/profile.tsx
@@ -1,33 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from '../components/navbar'
 import '../App.css'
 import './profile.css'
-import { useState, useEffect, Component } from "react";
-import { Multiselect } from "multiselect-react-dropdown";
-import { getOverlayDirection } from "react-bootstrap/esm/helpers";
-import { reduceEachLeadingCommentRange } from "typescript";
+import { Multiselect } from 'multiselect-react-dropdown'
+// import { getOverlayDirection } from 'react-bootstrap/esm/helpers'
+// import { reduceEachLeadingCommentRange } from 'typescript'
 
-export default function Profile() {
-const [allergies] = useState(["None", "Peanuts", "Tree Nuts", "Fish", "Eggs", "Soybeans", "Wheat", "Sesame", "Shellfish"]); 
-const [selects, setSelects] = useState(String);
-const [saved, setSave] = useState('');
-const [name, setName] = useState('');
-const [age, setAge] = useState('');
-const [height, setHeight] = useState('');
-const [weight, setWeight] = useState('');
-const [calories, setCalories] = useState('');
+export default function Profile () {
+  const [allergies] = useState(['None', 'Peanuts', 'Tree Nuts', 'Fish', 'Eggs', 'Soybeans', 'Wheat', 'Sesame', 'Shellfish'])
+  const [selects, setSelects] = useState(String)
+  const [name, setName] = useState('')
+  const [age, setAge] = useState('')
+  const [height, setHeight] = useState('')
+  const [weight, setWeight] = useState('')
+  const [calories, setCalories] = useState('')
 
-function saveProfile(){
-  var profileInfo = {
-    name: name,
-    age: age,
-    height: height,
-    weight: weight,
-    calories: calories,
-    selects: selects
+  function saveProfile () {
+    const profileInfo = {
+      name,
+      age,
+      height,
+      weight,
+      calories,
+      selects
+    }
+    localStorage.setItem('profileInfo', JSON.stringify(profileInfo))
   }
-  localStorage.setItem('profileInfo', JSON.stringify(profileInfo))
-}
 
   return (
     <div className="App">
@@ -37,32 +35,32 @@ function saveProfile(){
         <div className="profile-item">
           <h2>Profile Info</h2>
           <label htmlFor="profile-name">Name: </label>
-          <input type="text" name="profile-name" value={name} onChange={(e) =>{setName(e.target.value)}}/>
+          <input type="text" name="profile-name" value={name} onChange={(e) => { setName(e.target.value) }}/>
         </div>
 
         <div className="profile-item">
           <label htmlFor="profile-age">Age: </label>
-          <input type="text" name="profile-age" value={age} onChange={(e) =>{setAge(e.target.value)}}/>
+          <input type="text" name="profile-age" value={age} onChange={(e) => { setAge(e.target.value) }}/>
         </div>
 
         <div className="profile-item">
           <label htmlFor="profile-height">Height: </label>
-          <input type="text" name="profile-height" value={height} onChange={(e) =>{setHeight(e.target.value)}} />
+          <input type="text" name="profile-height" value={height} onChange={(e) => { setHeight(e.target.value) }} />
         </div>
 
         <div className="profile-item">
           <label htmlFor="profile-weight">Weight: </label>
-          <input type="text" name="profile-weight" value={weight} onChange={(e) =>{setWeight(e.target.value)}} />
+          <input type="text" name="profile-weight" value={weight} onChange={(e) => { setWeight(e.target.value) }} />
         </div>
 
         <div className="profile-item">
           <label htmlFor="profile-calories/day">Calories/Day: </label>
-          <input type="text" name="profile-calories/day" value={calories} onChange={(e) =>{setCalories(e.target.value)}}/>
+          <input type="text" name="profile-calories/day" value={calories} onChange={(e) => { setCalories(e.target.value) }}/>
         </div>
 
         <div className="profile-item">
           <label htmlFor="profile-dr">Dietary Restrictions: </label>
-          <select name="profile-dr" value={selects} onChange={(e) =>{setSelects(e.target.value)}}> 
+          <select name="profile-dr" value={selects} onChange={(e) => { setSelects(e.target.value) }}>
             <option value="select">Select</option>
             <option value="dairy-free">Dairy Free</option>
             <option value="keto">Keto</option>
@@ -99,7 +97,7 @@ function saveProfile(){
       <div className="profile-item">
         <button onClick={saveProfile}>Save</button>
       </div>
-        
+
       </div>
     </div>
   )
