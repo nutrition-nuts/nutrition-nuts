f9d92018dc01aeab786f5df116a801769025b1bb
Kyle Williams <kylemw1133@gmail.com>
2022-09-20 17:06:04 -0400

    basic elastic search added for workouts

--- packages/api/routes/workouts.ts
+++ packages/api/routes/workouts.ts
@@ -1,15 +1,43 @@
 import express from 'express'
 import fetchWorkouts from '../utils/fetchWorkouts.js'
+import elasticSearchClient from '../elastic/elastic-client'
 const router = express.Router()
 
 // GET /workouts/:bodyTarget
-router.get('/:bodyTarget', (req, res, next) => {
-  const workoutData = fetchWorkouts(req.params.bodyTarget)
-  console.log(workoutData)
-  res.send({
-    name: workoutData[0].name,
-    description: workoutData[0].description
-  })
+router.get('/:bodyTarget', async (req, res, next) => {
+  // TODO: delete this. just an example of how to hit the elasticsearch from code
+  let query = req.params.bodyTarget
+  let hits = await elasticSearchClient
+    .search({
+      index: 'workouts',
+      query: {
+        query_string: {
+          query: String(query) ?? 'press',
+        },
+      },
+    })
+    .then((value) => value.hits.hits.map((hit) => hit._source) ?? [])
+
+  // default case, give at least something back
+  if (hits.length === 0) {
+    hits = await elasticSearchClient
+      .search({
+        index: 'workouts',
+        query: {
+          query_string: {
+            query: 'bell',
+          },
+        },
+      })
+      .then((value) => value.hits.hits.map((hit) => hit._source) ?? [])
+  }
+
+  res.send(hits)
+
+  // res.send({
+  //   name: hits[0].name,
+  //   description: workoutData[0].description,
+  // })
 })
 
 export default router
