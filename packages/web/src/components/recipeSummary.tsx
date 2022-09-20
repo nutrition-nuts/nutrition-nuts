b4f96d40b9cef1f4a142296fc552fba5b03fb9b8
Jason Ahern <jason.ahern53@gmail.com>
2022-09-20 16:47:34 -0400

    a

--- packages/web/src/components/recipeSummary.tsx
+++ packages/web/src/components/recipeSummary.tsx
@@ -1,4 +1,8 @@
 import { RecipeModel } from '../models/recipeModels'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Modal from '@mui/material/Modal'
+import { useState } from 'react'
 
 interface Props {
   mealName: string
@@ -6,17 +10,51 @@ interface Props {
 }
 
 export default function RecipeSummary(props: Props) {
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4
+  }
+
   return (
     <>
       {props.recipe != null && (
         <>
           <hr />
-          <h3>
-            {props.mealName}: {props.recipe.title}
-          </h3>
+          <a href="#" onClick={handleOpen}>
+            <h3>
+              {props.mealName}: {props.recipe.title}
+            </h3>
+          </a>
           <div>{props.recipe.description}</div>
         </>
       )}
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
     </>
   )
 }
