a78d5c7c8e9a42769383774a8d611ed17a6afa29
Jason Ahern <jason.ahern53@gmail.com>
2022-09-22 16:11:32 -0400

    add requests for lunch and dinner

--- packages/web/src/routes/nutrition.tsx
+++ packages/web/src/routes/nutrition.tsx
@@ -12,10 +12,19 @@ import { getInputFieldValue } from '../utils/genericUtils'
 export default function Nutrition() {
   const [recipes, setRecipes] = useState<RecipeModel[]>()
   const [breakfastInput, setBreakfastInput] = useState('')
+  const [lunchInput, setLunchInput] = useState('')
+  const [dinnerInput, setDinnerInput] = useState('')
 
-  const makeGetRecipeRequest = async(query: string) => {
-    const res = await getRecipes(query)
-    setRecipes(res)
+  const makeGetRecipeRequest = async (query: string) => {
+    return await getRecipes(query)
+  }
+
+  const onFindRecipesButtonClick = async () => {
+    const breakfast = await makeGetRecipeRequest(breakfastInput)
+    const lunch = await makeGetRecipeRequest(lunchInput)
+    const dinner = await makeGetRecipeRequest(dinnerInput)
+
+    setRecipes([breakfast[0], lunch[0], dinner[0]])
   }
 
   return (
@@ -27,16 +36,33 @@ export default function Nutrition() {
           <h2>What are you feeling for...</h2>
 
           <label htmlFor="recipe-breakfast">Breakfast: </label>
-          <input type="text" name="recipe-breakfast" onChange={e => setBreakfastInput(getInputFieldValue(e))} />
+          <input
+            type="text"
+            name="recipe-breakfast"
+            onChange={(e) => setBreakfastInput(getInputFieldValue(e))}
+          />
           <br />
           <label htmlFor="recipe-lunch">Lunch: </label>
-          <input type="text" name="recipe-lunch"/>
+          <input
+            type="text"
+            name="recipe-lunch"
+            onChange={(e) => setLunchInput(getInputFieldValue(e))}
+          />
           <br />
           <label htmlFor="recipe-dinner">Dinner: </label>
-          <input type="text" name="recipe-dinner" />
+          <input
+            type="text"
+            name="recipe-dinner"
+            onChange={(e) => setDinnerInput(getInputFieldValue(e))}
+          />
           <br />
           <br />
-          <Button variant="contained" onClick={async() => await makeGetRecipeRequest(breakfastInput)}>Find me recipes!</Button>
+          <Button
+            variant="contained"
+            onClick={async () => await onFindRecipesButtonClick()}
+          >
+            Find me recipes!
+          </Button>
         </div>
         <div className="recipe-item">
           <h2>Meal Plan for the day</h2>
@@ -44,13 +70,10 @@ export default function Nutrition() {
             mealName="Breakfast"
             recipe={recipes?.[0]}
           ></RecipeSummary>
-          <RecipeSummary
-            mealName="Lunch"
-            recipe={recipes?.[0]}
-          ></RecipeSummary>
+          <RecipeSummary mealName="Lunch" recipe={recipes?.[1]}></RecipeSummary>
           <RecipeSummary
             mealName="Dinner"
-            recipe={recipes?.[0]}
+            recipe={recipes?.[2]}
           ></RecipeSummary>
         </div>
       </div>
