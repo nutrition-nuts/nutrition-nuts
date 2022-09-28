5521c158ee278ba7fad5103b1f7964f92a05d2e1
Jonathan Croxton <jccroxton@gmail.com>
2022-09-28 16:42:13 -0400

    pass through allergens from local storage

--- packages/web/src/requests/recipe.ts
+++ packages/web/src/requests/recipe.ts
@@ -1,8 +1,8 @@
-import { post } from './requests'
+import { get, post } from './requests'
 import { RecipeModel } from '../models/recipeModels'
 
 export const getRecipes = async(query: string, allergies: string) => {
-  const res = await post('/recipes', { query: query, allergies: allergies })
+  const res = await get('/recipes', { query: query, allergies: allergies })
   const recipes: RecipeModel[] = JSON.parse(JSON.stringify(res.data))
   return recipes
 }
