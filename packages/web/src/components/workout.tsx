35286e45ab6ffe614a5baad66474c091abb7f201
Nick Schauer <schauer.43@osu.edu>
2022-09-28 11:16:15 -0400

    Add modal pop-up to show full workout details

--- packages/web/src/components/workout.tsx
+++ packages/web/src/components/workout.tsx
@@ -1,13 +1,18 @@
 import { WorkoutModel } from '../models/workoutModels'
 import Pagination from '@mui/material/Pagination'
+import WorkoutModal from './workoutModal'
 import React, { useEffect, useState } from 'react'
+import '../App.css'
 
 interface Props {
   workouts: WorkoutModel[]
 }
 
 export default function Workout(props: Props) {
+  const [open, setOpen] = useState(false)
   const [page, setPage] = useState(1)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
 
   const handleChangePage = (
     event: React.ChangeEvent<unknown>,
@@ -23,27 +28,12 @@ export default function Workout(props: Props) {
       {props.workouts.length !== 0 && props.workouts.length >= page && (
           <>
             <div className="recipe-content">
-              <h3>{props.workouts[page - 1].name}</h3>
-              <p>Level: {props.workouts[page - 1].level}</p>
-              <p>
-                <h4>Primary Muscles:</h4>
-                <ul>
-                  {props.workouts[page - 1].primaryMuscles.map(function(object, i) {
-                    return <li key={i}>{object}</li>
-                  })}
-                </ul>
-              </p>
-              <p>
-                <h4>Secondary Muscles:</h4>
-                <ul>
-                  {props.workouts[page - 1].secondaryMuscles.map(function(object, i) {
-                    return <li key={i}>{object}</li>
-                  })}
-                </ul>
-              </p>
-              <p>
-                {props.workouts[page - 1].instructions}
-              </p>
+              <h3 onClick={handleOpen} className="header-link">
+                {props.workouts[page - 1].name}
+              </h3>
+              <p className={'caps'}>Type: {props.workouts[page - 1].category}</p>
+              <p className={'caps'}>Level: {props.workouts[page - 1].level}</p>
+              <p className={'caps'}>Primary Muscles: {props.workouts[page - 1].primaryMuscles[0]}</p>
             </div>
             <Pagination
                 count={props.workouts.length}
@@ -53,6 +43,11 @@ export default function Workout(props: Props) {
                 className="center"
                 style={{ marginTop: '1rem' }}
             />
+            <WorkoutModal
+                open={open}
+                handleClose={handleClose}
+                workout={props.workouts[page - 1]}
+            ></WorkoutModal>
           </>
       )}
     </>
