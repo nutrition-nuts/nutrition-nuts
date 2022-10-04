203f26458623d9eb72f85ab5075e32d9842d0726
Jason Ahern <jason.ahern53@gmail.com>
2022-10-04 17:08:11 -0400

    DATA

--- packages/elastic-search/utils/jsonArrayFileSeparator.py
+++ packages/elastic-search/utils/jsonArrayFileSeparator.py
@@ -1,12 +1,13 @@
 from os import listdir
 from os.path import isfile, join
 import json
-file = open('./recipe-data-test.json')
+file = open('./recipe-data.json')
 data = json.load(file)
 x = 0
 for i in data:
     json_obj = json.dumps(i, indent=4)
-    title = i["name"].replace(" ", "-").lower()
+    title = i["name"].replace(" ", "-").replace("/",
+                                                '-').replace("\'", "").replace("\"", "").lower()
     file_name = f"./data/recipes/{title}.json"
     with open(file_name, "w") as outfile:
         outfile.write(json_obj)
