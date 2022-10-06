1833ac2074f784af92a24653fc9b3453fa690bba
Nick Schauer <schauer.43@osu.edu>
2022-10-06 16:16:25 -0400

    Improved search results

--- packages/web/src/requests/youtubeSearch.ts
+++ packages/web/src/requests/youtubeSearch.ts
@@ -2,7 +2,7 @@ import { customGet } from './requests'
 import { VideoModel } from '../models/videoModel'
 
 export const getVideo = async(q: string, key: string) => {
-  const res = await customGet('https://youtube.googleapis.com/youtube/v3/search', { part: 'snippet', maxResults: '1', q: q + 'workout', key })
+  const res = await customGet('https://youtube.googleapis.com/youtube/v3/search', { part: 'snippet', maxResults: '1', q: 'how to do ' + q + ' workout', key })
   const requestJSON = JSON.parse(JSON.stringify(res.data))
   const it0 = requestJSON.items[0]
 
