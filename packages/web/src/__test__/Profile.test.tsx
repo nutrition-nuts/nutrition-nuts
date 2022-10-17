60dec6662f89bcc7885cfea5a763a7765eaba8fe
Jonathan Croxton <jccroxton@gmail.com>
2022-10-17 15:45:57 -0400

    everything funtional

--- packages/web/src/__test__/Profile.test.tsx
+++ packages/web/src/__test__/Profile.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
-import Profile from '../routes/profile'
+import { Profile } from '../routes/profile'
 
 test('renders the landing page', () => {
   const { getByText } = render(
