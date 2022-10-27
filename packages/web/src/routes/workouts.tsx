f71d02be07e63f1c7ba55d63269c11d1ac573ff2
Kyle Williams <kylemw1133@gmail.com>
2022-10-27 15:27:19 -0400

    added responsiveness to workout search

--- packages/web/src/routes/workouts.tsx
+++ packages/web/src/routes/workouts.tsx
@@ -5,9 +5,8 @@ import { getWorkout } from '../requests/workout'
 import Sidebar from '../components/sidebar'
 import Workout from '../components/workout'
 import { WorkoutModel } from '../models/workoutModels'
-import { Button, FormControl, MenuItem, Checkbox, FormControlLabel, Box } from '@mui/material'
+import { Grid, Button, FormControl, MenuItem, Checkbox, FormControlLabel } from '@mui/material'
 import StyledTextField from '../components/StyledTextField'
-
 export default function Workouts() {
   const [workouts, setWorkouts] = useState<WorkoutModel[][]>([])
   const [workoutType, setWorkoutType] = useState('')
@@ -29,71 +28,72 @@ export default function Workouts() {
   return (
     <div className="App">
       <Sidebar />
-
       <div className="recipe-container">
-        <div className="recipe-search">
-          <h2 id="search-header">Workout Info</h2>
-          <Box display="flex" style={{
-            alignItems: 'center',
-            justifyContent: 'center'
-          }}>
-            <StyledTextField
-                id="workout-type"
-                label="Workout Type"
-                sx={{ m: 1, minWidth: 200 }}
-                value={workoutType}
-                onChange={(e) => {
-                  setWorkoutType(e.target.value)
-                }}
-                select
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              {workoutTypes.map((type) => {
-                return <MenuItem value={type.toLowerCase()} key={type}>{type}</MenuItem>
-              })}
-            </StyledTextField>
-
-            <StyledTextField
-                id="muscle-group"
-                value={muscleInput}
-                label="Muscle Group"
-                variant="outlined"
-                sx={{ m: 1 }}
-                onChange={(e) => {
-                  setMuscleInput(e.target.value)
-                }}
-            ></StyledTextField>
-
-            <FormControl sx={{ m: 1, minWidth: 120 }}>
-              <FormControlLabel
-                control={
-                  <Checkbox
+        <div className = "recipe-search">
+        <h2 id="search-header">Workout Info</h2>
+        <Grid container >
+              <Grid item xs={12} md={3}>
+                <StyledTextField
+                    id="workout-type"
+                    label="Workout Type"
+                    sx={{ m: 1, minWidth: 200 }}
+                    value={workoutType}
                     onChange={(e) => {
-                      setEquipment(e.target.checked ? 'on' : 'off')
+                      setWorkoutType(e.target.value)
                     }}
-                    style={{
-                      color: '#506f8c'
-                    }}
+                    select
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  {workoutTypes.map((type) => {
+                    return <MenuItem value={type.toLowerCase()} key={type}>{type}</MenuItem>
+                  })}
+                </StyledTextField>
+              </Grid>
+              <Grid item xs={12} md={3}>
+                  <StyledTextField
+                      id="muscle-group"
+                      value={muscleInput}
+                      label="Muscle Group"
+                      variant="outlined"
+                      sx={{ m: 1 }}
+                      onChange={(e) => {
+                        setMuscleInput(e.target.value)
+                      }}
+                  ></StyledTextField>
+              </Grid>
+              <Grid item xs={12} md={3} >
+                <FormControl sx={{ m: 1, minWidth: 120 }}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        onChange={(e) => {
+                          setEquipment(e.target.checked ? 'on' : 'off')
+                        }}
+                        style={{
+                          color: '#506f8c'
+                        }}
+                      />
+                    }
+                    label="Equipment?"
                   />
-                }
-                label="Equipment?"
-              />
-            </FormControl>
-
-            <FormControl sx={{ m: 1 }}>
-              <Button
-                variant="contained"
-                onClick={async() => await onFindWorkoutsButtonClick()}
-                style={{
-                  background: '#506f8c'
-                }}
-              >
-                Find me a workout!
-              </Button>
-            </FormControl>
-          </Box>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} md= {3}>
+              <FormControl sx={{ m: 1 }}>
+                <Button
+                  variant="contained"
+                  onClick={async() => await onFindWorkoutsButtonClick()}
+                  style={{
+                    background: '#506f8c'
+                  }}
+                >
+                  Find me a workout!
+                </Button>
+              </FormControl>
+            </Grid>
+        </Grid>
         </div>
         <div className="recipe-item">
           <h2>Workout Plan</h2>
