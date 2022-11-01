7ff3e8119d0c6b43fc1674b6ce1c070f56b06447
Jason Ahern <jason.ahern53@gmail.com>
2022-11-01 17:15:11 -0400

    e2e with valid and invalid json

--- packages/api/__test__/schemas/requests/workouts/getWorkoutsRequest.test.ts
+++ packages/api/__test__/schemas/requests/workouts/getWorkoutsRequest.test.ts
@@ -1,6 +1,8 @@
 import getWorkoutsRequest from '../../../../schemas/requests/getWorkoutsRequest'
 import { invalidJson } from './invalidJson'
 import { validJson } from './validJson'
+import request from 'supertest'
+import app from '../../../../app'
 
 describe('Validate Get Recipes Request', () => {
   validJson.forEach((json) => {
@@ -11,6 +13,21 @@ describe('Validate Get Recipes Request', () => {
   invalidJson.forEach((json) => {
     test('Invalid Request', () => {
       expect(getWorkoutsRequest.validate(json)).toBeFalsy()
+    })
+  })
+})
+
+describe('E2E Server Tests for Valid and Invalid Workouts Requests', () => {
+  validJson.forEach((json) => {
+    test('Valid Request', async () => {
+      const res = await request(app).post('/workouts').send(json)
+      expect(res.statusCode).toEqual(200)
+    })
+  })
+  invalidJson.forEach((json) => {
+    test('Invalid Request', async () => {
+      const res = await request(app).post('/workouts').send(json)
+      expect(res.statusCode).toEqual(400)
     })
   })
 })
