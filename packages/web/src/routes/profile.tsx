1a1b209d7071437f62b6f030612efda8ffb0a035
Jonathan Croxton <jccroxton@gmail.com>
2022-11-05 22:07:16 -0400

    silders and tooltips

--- packages/web/src/routes/profile.tsx
+++ packages/web/src/routes/profile.tsx
@@ -8,10 +8,12 @@ import {
   resetDailyRecommendationToDefault
 } from '../utils/nutritionRecommendations'
 import { Nutrient } from '../utils/nutrient'
-import { Button, Tooltip, Zoom } from '@mui/material'
+import { Button, Tooltip, Zoom, IconButton } from '@mui/material'
 import { MultiSelectCheckMarks } from '../components/general/MultiSelectCheckmarks'
 import { possibleAllergies } from '../utils/allergy'
 import StyledTextField from '../components/StyledTextField'
+import NutritionSlider from './sliders/sliders'
+import InfoIcon from '@mui/icons-material/Info'
 
 function getAllergiesFromLocalStorage() {
   try {
@@ -21,20 +23,33 @@ function getAllergiesFromLocalStorage() {
   }
 }
 
+function getRatiosFromLocalStorage() {
+  try {
+    return JSON.parse(localStorage.getItem('macroRatios') ?? '')
+  } catch (error) {
+    return {
+      protein: 25,
+      carbs: 50,
+      fat: 25
+    }
+  }
+}
+
 class Form extends Component {
   state = {
     name: localStorage.getItem('name') ?? '',
     age: localStorage.getItem('age') ?? '',
-    calories: getNutrientDailyRecommendation(Nutrient.CALORIES).toString(),
-    protein: getNutrientDailyRecommendation(Nutrient.PROTEIN).toString(),
-    carbs: getNutrientDailyRecommendation(Nutrient.CARBOHYDRATES).toString(),
-    fat: getNutrientDailyRecommendation(Nutrient.FAT).toString(),
+    calories: getNutrientDailyRecommendation(Nutrient.CALORIES),
+    protein: getNutrientDailyRecommendation(Nutrient.PROTEIN),
+    carbs: getNutrientDailyRecommendation(Nutrient.CARBOHYDRATES),
+    fat: getNutrientDailyRecommendation(Nutrient.FAT),
     dr: localStorage.getItem('dr') ?? '',
     allergies: getAllergiesFromLocalStorage() as string[],
     saveProfile: false,
     showForm: true,
     count: 0,
-    possibleAllergies: ['Peanuts', 'Tree Nuts', 'Fish', 'Eggs', 'Soy']
+    possibleAllergies: ['Peanuts', 'Tree Nuts', 'Fish', 'Eggs', 'Soy'],
+    macroRatios: getRatiosFromLocalStorage()
   }
 
   handleChange = (event: { target: { name: any, value: any } }) => {
@@ -55,6 +70,7 @@ class Form extends Component {
     localStorage.setItem('age', this.state.age)
     localStorage.setItem('dr', this.state.dr)
     // localStorage.setItem('allergies', this.state.allergies)
+    localStorage.setItem('macroRatios', JSON.stringify(this.state.macroRatios))
     this.updateMacroGoals()
     localStorage.setItem('allergies', JSON.stringify(this.state.allergies))
     this.setState({
@@ -78,10 +94,14 @@ class Form extends Component {
   }
 
   updateMacroGoals = () => {
+    // normalize inputs so they don't have to add up to 100%
+    const total = Object.values(this.state.macroRatios).reduce((acc, x) => Number(acc) + Number(x), 0)
+    const cals = this.state.calories / Number(total)
+
     setNutrientDailyRecommendation(Nutrient.CALORIES, +this.state.calories)
-    setNutrientDailyRecommendation(Nutrient.PROTEIN, +this.state.protein)
-    setNutrientDailyRecommendation(Nutrient.CARBOHYDRATES, +this.state.carbs)
-    setNutrientDailyRecommendation(Nutrient.FAT, +this.state.fat)
+    setNutrientDailyRecommendation(Nutrient.PROTEIN, +Math.floor(cals * this.state.macroRatios.protein / 4))
+    setNutrientDailyRecommendation(Nutrient.CARBOHYDRATES, +Math.floor(cals * this.state.macroRatios.carbs / 4))
+    setNutrientDailyRecommendation(Nutrient.FAT, +Math.floor(cals * this.state.macroRatios.fat / 9))
   }
 
   resetMacroToDefault = (nutrient: Nutrient) => {
@@ -98,6 +118,14 @@ class Form extends Component {
 
   onAllergyMultiSelectChangeCallback = (allergies: string[]) => {
     this.setState({ allergies })
+  }
+
+  onSliderChangeCallback = (name: string, value: number) => {
+    this.setState((state) => {
+      const temp = Object(state).macroRatios
+      temp[name] = value
+      return { macroRatios: temp }
+    })
   }
 
   render() {
@@ -190,8 +218,46 @@ class Form extends Component {
                   </div>
 
                   <div className="profile-item">
-                    <label htmlFor="protein">Daily Protein Intake (g): </label>
-                    <StyledTextField
+                    <label htmlFor="protein">Daily Protein Intake
+                    <Tooltip
+                    arrow
+                    disableFocusListener
+                    disableTouchListener
+                    title={
+                      <div>
+                      Protein, made of amino acids, is used to build tissue in your body.
+                      <br></br>
+                      <br></br>
+                      Below 20% of the diet, you may not have enough to maintain and build muscle.
+                      <br></br>
+                      <br></br>
+                      Extra protein above 30% gets mostly wasted in the urine,
+                      and can cause ammonia buildup if kidney or liver function is impaired.
+                    </div>
+                    }
+                    TransitionComponent={Zoom}
+                  >
+                    <IconButton>
+                      <InfoIcon
+                        sx={{
+                          color: 'white'
+                        }}
+                      />
+                    </IconButton>
+                  </Tooltip>
+                    </label>
+                    <NutritionSlider
+                      name='protein'
+                      // macro={Nutrient.PROTEIN}
+                      min={10}
+                      max={40}
+                      sliderValue={this.state.macroRatios.protein ?? 25}
+                      lowerMark={20}
+                      upperMark={30}
+                      // onChange={this.handleChange}
+                      onChangeCallback={this.onSliderChangeCallback}
+                    />
+                    {/* <StyledTextField
                       type="number"
                       inputProps={{
                         min: '0',
@@ -217,14 +283,51 @@ class Form extends Component {
                       }}
                     >
                       Reset to default
-                    </Button>
+                    </Button> */}
                   </div>
 
                   <div className="profile-item">
                     <label htmlFor="carbs">
-                      Daily Carbohydrate Intake (g):{' '}
+                      Daily Carbohydrate Intake
+                      <Tooltip
+                    arrow
+                    disableFocusListener
+                    disableTouchListener
+                    title={
+                      <div>
+                      Carbohydrates—sugars and starches—are quick sources of energy thanks to their high solubility in water.  They are the preferred fuel source
+                      for the brain, the liver, and the muscles during high-intensity exercise.
+                      <br></br>
+                      <br></br>
+                      The body can create enough carbohydrates endogenously (from protein) to survive.  However, doing so raises stress hormones such as prolactin and cortisol.
+                      Since the brain uses almost exclusively carbohydrates, and accounts for 20% of the body&apos;s energy needs, we suggest setting this to at least 20%.
+                      <br></br>
+                      <br></br>
+                      High carbohydrate diets are generally well tolerated except by diabetics.
+                    </div>
+                    }
+                    TransitionComponent={Zoom}
+                  >
+                    <IconButton>
+                      <InfoIcon
+                        sx={{
+                          color: 'white'
+                        }}
+                      />
+                    </IconButton>
+                  </Tooltip>
                     </label>
-                    <StyledTextField
+                    <NutritionSlider
+                      name='carbs'
+                      // macro={Nutrient.CARBOHYDRATES}
+                      min={0}
+                      max={70}
+                      sliderValue={this.state.macroRatios.carbs ?? 50}
+                      lowerMark={20}
+                      upperMark={60}
+                      onChangeCallback={this.onSliderChangeCallback}
+                    />
+                    {/* <StyledTextField
                       type="number"
                       inputProps={{
                         min: '0',
@@ -250,12 +353,49 @@ class Form extends Component {
                       }}
                     >
                       Reset to default
-                    </Button>
+                    </Button> */}
                   </div>
 
                   <div className="profile-item">
-                    <label htmlFor="fat">Daily Fat Intake (g): </label>
-                    <StyledTextField
+                    <label htmlFor="fat">Daily Fat Intake
+                    <Tooltip
+                    arrow
+                    disableFocusListener
+                    disableTouchListener
+                    title={
+                      <div>
+                      Fats are a denser source of energy, and are the muscles&apos; preferred fuel source at rest.  They are also used throughout the body for structural purposes.
+                      <br></br>
+                      <br></br>
+                      The body can create enough fat endogenously to survive on a very low-fat diet; however, this may not be optimal due to fat&apos;s role in steroid hormone synthesis.
+                      <br></br>
+                      <br></br>
+                      High-fat diets are generally well tolerated, assuming other nutrients are not restricted.  However, fat metabolism produces less carbon dioxide
+                      than carbohydrate metabolism, which causes lower tissue oxygenation.  So, high-fat diets may exacerbate symptoms of poor red blood cell or lung function.
+                    </div>
+                    }
+                    TransitionComponent={Zoom}
+                  >
+                    <IconButton>
+                      <InfoIcon
+                        sx={{
+                          color: 'white'
+                        }}
+                      />
+                    </IconButton>
+                  </Tooltip>
+                    </label>
+                    <NutritionSlider
+                      name='fat'
+                      // macro={Nutrient.FAT}
+                      min={0}
+                      max={70}
+                      sliderValue={this.state.macroRatios.fat ?? 25}
+                      lowerMark={5}
+                      upperMark={50}
+                      onChangeCallback={this.onSliderChangeCallback}
+                    />
+                    {/* <StyledTextField
                       type="number"
                       inputProps={{
                         min: '0',
@@ -279,7 +419,7 @@ class Form extends Component {
                       }}
                     >
                       Reset to default
-                    </Button>
+                    </Button> */}
                   </div>
 
                   {/* <div className="profile-item">
---
+++ packages/web/src/routes/sliders/sliders.tsx
@@ -0,0 +1,85 @@
+import { Slider } from '@mui/material'
+import { useState } from 'react'
+
+interface NutritionSliderProps {
+  name: string
+  // macro: Nutrient
+  min: number
+  max: number
+  lowerMark: number
+  upperMark: number
+  sliderValue: number
+  onChangeCallback: (name: string, value: number) => void
+}
+
+function percentify(x: number) {
+  return String(x).concat('%')
+}
+
+export default function NutritionSlider(props: NutritionSliderProps) {
+  const { name, min, max, lowerMark, upperMark } = props
+  const [sliderValue, setValue] = useState<number>(props.sliderValue)
+  const defaultVal = sliderValue
+
+  const handleChange = (
+    event: Event,
+    value: number | number[]
+    // activeThumb: number
+  ) => {
+    setValue(Number(value))
+    props.onChangeCallback(name, sliderValue)
+  }
+
+  return (
+       <div className="profile-item">
+        {/* <Box sx={{ width: 400, alignContent: 'center' }}> */}
+          <Slider
+            aria-label={name}
+            defaultValue={defaultVal}
+            value={sliderValue}
+            // getAriaValueText={valuetext}
+            step={1}
+            valueLabelDisplay="on"
+            marks={
+                [
+                  {
+                    value: min,
+                    label: percentify(min)
+                  },
+                  {
+                    value: lowerMark,
+                    label: percentify(lowerMark)
+                  },
+                  {
+                    value: upperMark,
+                    label: percentify(upperMark)
+                  },
+                  {
+                    value: max,
+                    label: percentify(max)
+                  }
+                ]
+            }
+            min={min}
+            max={max}
+            name={name}
+            onChange={handleChange}
+            orientation='horizontal'
+            valueLabelFormat={(x) => String(x).concat('%')}
+            // slot={<div></div>}
+            // slotProps={oninput: null}
+            sx={
+              {
+                '& .MuiSlider-markLabel': {
+                  color: 'white'
+                },
+                width: String(max).concat('%')
+              }}
+          >
+          </Slider>
+        {/* </Box> */}
+      </div>
+  )
+}
+
+export { NutritionSlider }
